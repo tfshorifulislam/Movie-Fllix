21e648a9d35ebde02ea20324f6c298979e5bbd53
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-28 16:20:48 +0600

    button add and dynamic function add for css

---
+++ src/Components/MovieCard/MovieCard.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const MovieCard = ({ movie }) => {
+    const [addCart, setAddCart] = useState(false)
+    return (
+        <div>
+            <div>
+                <div className="border-2 border-[#00000025] rounded-2xl shadow-lg md:shadow-xl p-7">
+                    <figure>
+                        <img className='rounded-sm md:rounded-2xl'
+                            src={movie.image}
+                            alt={movie.title} />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{movie.title}</h2>
+                        <p>{movie.description}</p>
+
+
+                    </div>
+                    <div className='flex justify-between items-center'>
+                        <button
+                            onClick={() => setAddCart(true)}
+                            className={`btn  text-white shadow-2xl duration-500
+                                ${addCart === true ?
+                                    'bg-gray-500'
+                                    : 'bg-purple-700'
+                                }`}>
+                            {
+                                addCart === true ?
+                                    'Success'
+                                    : 'Add to Cart'
+                            }
+                        </button>
+                        <p className='font-bold py-2 px-3 rounded-md border'>Price: {movie.price}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
--- src/Components/MovieData/MovieData.jsx
+++ src/Components/MovieData/MovieData.jsx
@@ -1,38 +1,28 @@
 import React, { use } from 'react';
-import { FaCartArrowDown } from 'react-icons/fa';
-
+import { CiShoppingCart } from 'react-icons/ci';
+import { FaFire } from 'react-icons/fa';
+import MovieCard from '../MovieCard/MovieCard';
 
 const MovieData = ({ movieDataRes }) => {
+
+
     const allMovieData = use(movieDataRes);
     console.log(allMovieData);
     return (
         <div className='w-11/12 mx-auto'>
-            <div className='flex justify-between items-center font-bold text-2xl md:text-4xl my-3 md:my-6 '>
-                <h1>Trending Now</h1>
-                <FaCartArrowDown className='btn' />
+            <div className='flex justify-between items-center font-bold text-2xl md:text-4xl my-10'>
+                <div className='flex gap-2'>
+                    <h1>Trending Now</h1>
+                    <FaFire className='text-purple-700 animate-pulse' />
+                </div>
+                <div className='relative btn'>
+                    <CiShoppingCart className='text-4xl' />
+                    <div class="badge bg-purple-700 text-white absolute right-10 -top-3">0</div>
+                </div>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
                 {
-                    allMovieData.map(movie =>
-                        <div>
-                            <div className="border-2 border-[#00000025] rounded-2xl shadow-lg md:shadow-xl p-7">
-                                <figure>
-                                    <img className='rounded-sm md:rounded-2xl'
-                                        src={movie.image}
-                                        alt={movie.title} />
-                                </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{movie.title}</h2>
-                                    <p>{movie.description}</p>
-
-
-                                </div>
-                                    <div className='flex justify-between items-center'>
-                                        <button className='btn bg-purple-700 text-white shadow-2xl'>Add to Cart</button>
-                                        <p className='font-bold py-2 px-3 rounded-md border'>Price: {movie.price}</p>
-                                    </div>
-                            </div>
-                        </div>)
+                    allMovieData.map(movie => <MovieCard movie={movie} /> )
                 }
             </div>
         </div>
