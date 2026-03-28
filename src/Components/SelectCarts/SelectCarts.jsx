c9945ef26a32f2d9b07813ae07b9ec5fde3c0270
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-29 03:59:17 +0600

    empty message responsive issue fixed

--- src/Components/SelectCarts/SelectCarts.jsx
+++ src/Components/SelectCarts/SelectCarts.jsx
@@ -9,8 +9,8 @@ const SelectCarts = ({ cartsData, setSection, setSelectedCart }) => {
             {
                 cartsData.length === 0 ?
                     (<div className='flex flex-col justify-center items-center my-20'>
-                        <p className='font-bold text-5xl mt-28'>Your cart is empty!</p>
-                        <p className='font-medium text-2xl mt-5 text-purple-700 animate-pulse btn'
+                        <p className='font-bold text-3xl md:text-5xl mt-28 '>Your cart is empty!</p>
+                        <p className='font-medium text-2xl mt-3 md:mt-5 text-purple-700 animate-pulse btn'
                             onClick={() => { setSection('Home'), setSelectedCart(false) }}>
                             Go to Home page
                         </p>
